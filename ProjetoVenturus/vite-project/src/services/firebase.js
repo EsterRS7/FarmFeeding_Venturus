d14a35a80f8442b84a5889143703db4ea59c0f66
Guzhie <133404058+Guzhie@users.noreply.github.com>
2025-05-07 20:50:57 -0300

    alteração das informações do banco

--- ProjetoVenturus/vite-project/src/services/firebase.js
+++ ProjetoVenturus/vite-project/src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpvoo3UX_ihxt3yHY95DWqJHk6a2EXsIc",
-    authDomain: "farmfeed-7b9f2.firebaseapp.com",
-    projectId: "farmfeed-7b9f2",
-    storageBucket: "farmfeed-7b9f2.firebasestorage.app",
-    messagingSenderId: "372930664551",
-    appId: "1:372930664551:web:15a1d384fd55b3291ddcc2"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
   };
 
 const app = initializeApp(firebaseConfig);
